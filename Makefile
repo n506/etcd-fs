6373e90b33aed91a9d24102b281810638f762e80
Dmitry Kutepov <n506@n506.me>
2016-07-11 19:42:49 +0300

    Another startup rework + checks

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ test:
 	go get github.com/franela/goblin
 	go get github.com/coreos/go-etcd/etcd
 	go get github.com/hanwen/go-fuse/fuse
+	go get github.com/jessevdk/go-flags
 	go test -v etcdfs
 
 install:
@@ -14,4 +15,5 @@ build:
 	go get github.com/franela/goblin
 	go get github.com/coreos/go-etcd/etcd
 	go get github.com/hanwen/go-fuse/fuse
+	go get github.com/jessevdk/go-flags
 	go build src/github.com/xetorthio/etcd-fs/mount/etcd-fs.go
--- src/github.com/xetorthio/etcd-fs/mount/etcd-fs.go
+++ src/github.com/xetorthio/etcd-fs/mount/etcd-fs.go
@@ -1,56 +1,87 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"os"
-        "strings"
+    "log"
+    "os"
+    "strings"
+    "bytes"
+    "fmt"
 
-	"github.com/hanwen/go-fuse/fuse/nodefs"
-	"github.com/hanwen/go-fuse/fuse/pathfs"
-	etcdfs "github.com/xetorthio/etcd-fs/fs"
+    "github.com/hanwen/go-fuse/fuse/nodefs"
+    "github.com/hanwen/go-fuse/fuse/pathfs"
+    etcdfs "github.com/xetorthio/etcd-fs/fs"
+    "github.com/jessevdk/go-flags"
 )
 
 type ConfigT struct {
-    verbose bool
-    end string
-    mount string
+    Verbose bool `short:"v" long:"verbose" description:"verbose debug output" env:"ETCD_FS_VERBOSE"`
+    End string `short:"e" long:"endpoint" description:"ETCD endpoints, comma separated" default:"http://localhost:2739" env:"ETCD_FS_ENDPOINT"`
+    Mount string `short:"m" long:"mount" description:"mountpoint for fs, must be created.  REQUIRED" required:"true" env:"ETCD_FS_MOUNT"`
+    Help bool `short:"h" long:"help" description:"Show this help message"`
 }
 
 var Config ConfigT
 
-func init() {
-    flag.StringVar(&Config.mount, "mount", "", "mountpoint for fs, REQUIRED")
-    flag.StringVar(&Config.end, "endpoint", "http://localhost:2739", "ETCD endpoints, comma separated")
-    flag.BoolVar(&Config.verbose, "verbose", false, "verbose debug output")
-}
-
 func main() {
-        flag.Parse()
 
-        if Config.mount == "" {
-            flag.Usage()
-            os.Exit(1)
-        }
+    parser := flags.NewParser(&Config, flags.PrintErrors | flags.PassDoubleDash)
+    if _, err := parser.Parse(); err != nil {
+        var b bytes.Buffer
+        parser.WriteHelp(&b)
+        fmt.Fprintf(os.Stderr, "%s", &b)
+        os.Exit(1)
+    }
 
-        defer log.Println("etcd-fs stopped")
+    if Config.Help == true {
+        var b bytes.Buffer
+        parser.WriteHelp(&b)
+        fmt.Fprintf(os.Stderr, "%s", &b)
+        os.Exit(1)
+    }
 
-        endpoints := strings.Split(Config.end, ",")
+    if Config.Mount == "" {
+        fmt.Fprint(os.Stderr, "Mountpoint is empty\n")
+        var b bytes.Buffer
+        parser.WriteHelp(&b)
+        fmt.Fprintf(os.Stderr, "%s", &b)
+        os.Exit(1)
+    }
+
+    if _, err := os.Stat(Config.Mount); os.IsNotExist(err) {
+        fmt.Fprint(os.Stderr, "Mountpoint does not exists\n")
+        var b bytes.Buffer
+        parser.WriteHelp(&b)
+        fmt.Fprintf(os.Stderr, "%s", &b)
+        os.Exit(1)
+    }
+
+    endpoints := strings.Split(Config.End, ",")
+
+    if len(endpoints) == 0  || endpoints[0] == "" {
+        fmt.Fprint(os.Stderr, "Endpoints list is empty\n")
+        var b bytes.Buffer
+        parser.WriteHelp(&b)
+        fmt.Fprintf(os.Stderr, "%s", &b)
+        os.Exit(1)
+    }
+
+    log.Printf("ETCD endpoints: %v\n", endpoints)
+    log.Printf("Mountpoint: %v\n", Config.Mount)
+    log.Printf("Verbose output: %v\n", Config.Verbose)
+
+
+    etcdFs := etcdfs.EtcdFs{
+        FileSystem:   pathfs.NewDefaultFileSystem(),
+        EtcdEndpoint: endpoints,
+        Verbose: Config.Verbose,
+    }
+    nfs := pathfs.NewPathNodeFs(&etcdFs, nil)
+    server, _, err := nodefs.MountRoot(Config.Mount, nfs.Root(), nil)
+    if err != nil {
+        log.Fatalf("Mount fail: %v", err)
+    } else {
         log.Println("etcd-fs started")
-        log.Printf("ETCD endpoints: %v\n", endpoints)
-        log.Printf("Mountpoint: %v\n", Config.mount)
-        log.Printf("Verbose output: %v\n", Config.verbose)
-
-	etcdFs := etcdfs.EtcdFs{
-		FileSystem:   pathfs.NewDefaultFileSystem(),
-		EtcdEndpoint: endpoints,
-                Verbose: Config.verbose,
-	}
-	nfs := pathfs.NewPathNodeFs(&etcdFs, nil)
-	server, _, err := nodefs.MountRoot(flag.Arg(0), nfs.Root(), nil)
-	if err != nil {
-		log.Printf("Mount fail: %v", err)
-	} else {
-	    server.Serve()
-        }
+        defer log.Println("etcd-fs stopped")
+        server.Serve()
+    }
 }
