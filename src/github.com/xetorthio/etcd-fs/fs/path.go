851bea3d51e5e80d74c92809a5525e67f3b61b03
Dmitry Kutepov <n506@n506.me>
2016-07-12 17:23:07 +0300

    add fuse verbose debug

--- src/github.com/xetorthio/etcd-fs/fs/path.go
+++ src/github.com/xetorthio/etcd-fs/fs/path.go
@@ -1,11 +1,11 @@
 package etcdfs
 
 import (
-        "bytes"
+    "bytes"
     "log"
     "strings"
     "sync"
-        "time"
+    "time"
 
     "github.com/hanwen/go-fuse/fuse"
     "github.com/hanwen/go-fuse/fuse/nodefs"
@@ -16,11 +16,36 @@ import (
 
 type EtcdFs struct {
     pathfs.FileSystem
-    EtcdEndpoint []string
-        Verbose bool
+    EtcdEndpoint        []string
+    Verbose             bool
+    connlock            sync.Mutex
+    connection          *etcd.Client
+}
 
-    connlock   sync.RWMutex
-    connection *etcd.Client
+var Status map[fuse.Status]string
+
+func init() {
+    Status = make(map[fuse.Status]string)
+    Status[fuse.OK]      = "OK"
+    Status[fuse.EACCES]  = "EACCES"
+    Status[fuse.EBUSY]   = "EBUSY"
+    Status[fuse.EINVAL]  = "EINVAL"
+    Status[fuse.EIO]     = "EIO"
+    Status[fuse.ENOENT]  = "ENOENT"
+    Status[fuse.ENOSYS]  = "ENOSYS"
+    Status[fuse.ENODATA] = "ENODATA"
+    Status[fuse.ENOTDIR] = "ENOTDIR"
+    Status[fuse.EPERM]   = "EPERM"
+    Status[fuse.ERANGE]  = "ERANGE"
+    Status[fuse.EXDEV]   = "EXDEV"
+    Status[fuse.EBADF]   = "EBADF"
+    Status[fuse.ENODEV]  = "ENODEV"
+    Status[fuse.EROFS]   = "EROFS"
+}
+
+func (me *EtcdFs) logfuse(s string, i fuse.Status) fuse.Status {
+    if me.Verbose {log.Printf("%s: %i/%s", s, i, Status[i])}
+    return i
 }
 
 func (me *EtcdFs) NewEtcdClient() *etcd.Client {
@@ -28,72 +53,83 @@ func (me *EtcdFs) NewEtcdClient() *etcd.Client {
     defer me.connlock.Unlock()
 
     if me.connection == nil {
+            if me.Verbose {log.Println("Make new ETCD connection")}
         me.connection = etcd.NewClient(me.EtcdEndpoint)
     }
     return me.connection
 }
 
 func (me *EtcdFs) String() string {
-        return "etcd-fs"
+    return "etcd-fs"
 }
 
 func (me *EtcdFs) Unlink(name string, context *fuse.Context) (code fuse.Status) {
+    if me.Verbose {log.Printf("Unlink: %v\n", name)}
+
     if name == "" {
-        return fuse.OK
+        return me.logfuse("Unlink", fuse.OK)
     }
 
     _, err := me.NewEtcdClient().Delete(name, false)
 
     if err != nil {
         log.Println(err)
-        return fuse.ENOENT
+        return me.logfuse("Unlink", fuse.ENOENT)
     }
 
-    return fuse.OK
+    return me.logfuse("Unlink", fuse.OK)
 }
 
 func (me *EtcdFs) Rmdir(name string, context *fuse.Context) (code fuse.Status) {
+    if me.Verbose {log.Printf("Rmdir: %v\n", name)}
+
     if name == "" {
-        return fuse.OK
+        return me.logfuse("Rmdir", fuse.OK)
     }
 
     _, err := me.NewEtcdClient().RawDelete(name, true, true)
 
     if err != nil {
         log.Println(err)
-        return fuse.ENOENT
+        return me.logfuse("Rmdir", fuse.ENOENT)
     }
 
-    return fuse.OK
+    return me.logfuse("Rmdir", fuse.OK)
 }
 
 func (me *EtcdFs) Create(name string, flags uint32, mode uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
+    if me.Verbose {log.Printf("Create: %v, %v, %v\n", name, flags, mode)}
+
     _, err := me.NewEtcdClient().Set(name, "", 0)
 
     if err != nil {
         log.Println("Create Error:", err)
-        return nil, fuse.ENOENT
+        return nil, me.logfuse("Create", fuse.ENOENT)
     }
 
-    return NewEtcdFile(me.NewEtcdClient(), name), fuse.OK
+    return NewEtcdFile(me.NewEtcdClient(), name), me.logfuse("Create", fuse.OK)
 }
 
 func (me *EtcdFs) Mkdir(name string, mode uint32, context *fuse.Context) fuse.Status {
+    if me.Verbose {log.Printf("Mkdir: %v, %v\n", name, mode)}
+
     if name == "" {
-        return fuse.OK
+        return me.logfuse("Mkdir", fuse.OK)
     }
 
     _, err := me.NewEtcdClient().CreateDir(name, 0)
 
     if err != nil {
         log.Println(err)
-        return fuse.ENOENT
+        return me.logfuse("Mkdir", fuse.ENOENT)
     }
 
-    return fuse.OK
+    return me.logfuse("Mkdir", fuse.OK)
 }
 
 func (me *EtcdFs) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.Status) {
+    if me.Verbose {log.Printf("GetAttr: %v\n", name)}
+
     if name == "" {
         return &fuse.Attr{
             Mode: fuse.S_IFDIR | 0666,
@@ -103,7 +139,7 @@ func (me *EtcdFs) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.
     res, err := me.NewEtcdClient().Get(name, false, false)
 
     if err != nil {
-        return nil, fuse.ENOENT
+        return nil, me.logfuse("GetAttr", fuse.ENOENT)
     }
 
     var attr fuse.Attr
@@ -118,15 +154,17 @@ func (me *EtcdFs) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.
         }
     }
 
-    return &attr, fuse.OK
+    return &attr, me.logfuse("GetAttr", fuse.OK)
 }
 
 func (me *EtcdFs) OpenDir(name string, context *fuse.Context) (c []fuse.DirEntry, code fuse.Status) {
+    if me.Verbose {log.Printf("OpenDir: %v\n", name)}
+
     res, err := me.NewEtcdClient().Get(name, false, false)
 
     if err != nil {
         log.Println("OpenDir Error:", err)
-        return nil, fuse.ENOENT
+        return nil, me.logfuse("OpenDir", fuse.ENOENT)
     }
 
     entries := []fuse.DirEntry{}
@@ -141,110 +179,126 @@ func (me *EtcdFs) OpenDir(name string, context *fuse.Context) (c []fuse.DirEntry
         }
     }
 
-    return entries, fuse.OK
+    return entries, me.logfuse("OpenDir", fuse.OK)
 }
 
 func (me *EtcdFs) Open(name string, flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
+    if me.Verbose {log.Printf("Open: %v, %v\n", name, flags)}
+
     _, err := me.NewEtcdClient().Get(name, false, false)
 
     if err != nil {
         log.Println("Open Error:", err)
-        return nil, fuse.ENOENT
+        return nil, me.logfuse("Open", fuse.ENOENT)
     }
 
-    return NewEtcdFile(me.NewEtcdClient(), name), fuse.OK
+    return NewEtcdFile(me.NewEtcdClient(), name), me.logfuse("Open", fuse.OK)
 }
 
 func (me *EtcdFs) Rename(oldName string, newName string, context *fuse.Context) (code fuse.Status) {
-        etcdClient := me.NewEtcdClient()
-        res, err := etcdClient.Get(oldName, false, false)
-        if err != nil {
-            log.Println(err)
-            return fuse.ENOENT
-        }
-        originalValue := []byte(res.Node.Value)
-        newValue := bytes.NewBuffer(originalValue)
-        if _, err :=etcdClient.Set(newName, newValue.String(), 0); err != nil {
-            log.Println(err)
-            return fuse.ENOENT
-        }
-        if _, err := etcdClient.Delete(oldName, false); err != nil {
-            log.Println(err)
-            etcdClient.Delete(newName, false)
-            return fuse.ENOENT
-        }
+    if me.Verbose {log.Printf("Rename: %v -> %v\n", oldName, newName)}
 
-        return fuse.OK
+    etcdClient := me.NewEtcdClient()
+    res, err := etcdClient.Get(oldName, false, false)
+    if err != nil {
+        log.Println(err)
+        return me.logfuse("Rename", fuse.ENOENT)
+    }
+    originalValue := []byte(res.Node.Value)
+    newValue := bytes.NewBuffer(originalValue)
+    if _, err :=etcdClient.Set(newName, newValue.String(), 0); err != nil {
+        log.Println(err)
+        return me.logfuse("Rename", fuse.ENOENT)
+    }
+    if _, err := etcdClient.Delete(oldName, false); err != nil {
+        log.Println(err)
+        etcdClient.Delete(newName, false)
+        return me.logfuse("Rename", fuse.ENOENT)
+    }
+        return me.logfuse("Rename", fuse.OK)
 }
 
 func (me *EtcdFs) Access(name string, mode uint32, context *fuse.Context) (code fuse.Status) {
-        etcdClient := me.NewEtcdClient()
-        _, err := etcdClient.Get(name, false, false)
-        if err != nil {
-            log.Println(err)
-            return fuse.ENOENT
-        }
-        return fuse.OK
+    if me.Verbose {log.Printf("Access: %v, %v\n", name, mode)}
+
+    etcdClient := me.NewEtcdClient()
+    _, err := etcdClient.Get(name, false, false)
+    if err != nil {
+        log.Println(err)
+        return me.logfuse("Access", fuse.ENOENT)
+    }
+    return me.logfuse("Access", fuse.OK)
 }
 
 func (me *EtcdFs) Link(oldName string, newName string, context *fuse.Context) (code fuse.Status) {
-        return fuse.ENOSYS
+    if me.Verbose {log.Printf("Link: %v, %v\n", oldName, newName)}
+    return me.logfuse("Link", fuse.ENOSYS)
 }
 
 func (me *EtcdFs) Symlink(name string, linkName string, context *fuse.Context) (fuse.Status) {
-        return fuse.ENOSYS
+    if me.Verbose {log.Printf("Symlink: %v\n", name)}
+    return me.logfuse("Link", fuse.ENOSYS)
 }
 
 func (me *EtcdFs) Readlink(name string, context *fuse.Context) (string, fuse.Status) {
-        return "", fuse.ENOSYS
+    if me.Verbose {log.Printf("Readlink: %v\n", name)}
+    return "", me.logfuse("ReadLink", fuse.ENOSYS)
 }
 
 func (me *EtcdFs) Mknod(name string, mode uint32, dev uint32, context *fuse.Context) fuse.Status {
-        return fuse.ENOSYS
+    if me.Verbose {log.Printf("Mknod: %v, %v, %v\n", name, mode, dev)}
+    return me.logfuse("Mknod", fuse.ENOSYS)
 }
 
 func (me *EtcdFs) Utimens(name string, atime *time.Time, mtime *time.Time, context *fuse.Context) (code fuse.Status) {
-        return fuse.OK
+    if me.Verbose {log.Printf("Utimens: %v, %v, %v\n", name, atime, mtime)}
+    return me.logfuse("Utimens", fuse.OK)
 }
 
 func (me *EtcdFs) Chmod(name string, perms uint32, context *fuse.Context) (code fuse.Status) {
-        return fuse.OK
+    if me.Verbose {log.Printf("Chmod: %v, %v\n", name, perms)}
+    return me.logfuse("Chmod", fuse.OK)
 }
 
 func (me *EtcdFs) Chown(name string, uid uint32, gid uint32, context *fuse.Context) (code fuse.Status) {
-        return fuse.OK
+    if me.Verbose {log.Printf("Chown: %v, %v, %v\n", name, uid, gid)}
+    return me.logfuse("Chown", fuse.OK)
 }
 
 func (me *EtcdFs) Truncate(name string, size uint64, context *fuse.Context) (code fuse.Status) {
-        etcdClient := me.NewEtcdClient()
+    if me.Verbose {log.Printf("Truncate: %v, %v\n", name, size)}
+    etcdClient := me.NewEtcdClient()
 
-        _, err := etcdClient.Get(name, false, false)
-
-        if err != nil {
-            log.Println(err)
-            return fuse.EIO
-        }
-        if _, err := etcdClient.Set(name, "", 0); err != nil {
-            log.Println(err)
-            return fuse.EIO
-        }
-        return fuse.OK
+    _, err := etcdClient.Get(name, false, false)
+    if err != nil {
+        log.Println(err)
+        return me.logfuse("Truncate", fuse.EIO)
+    }
+    if _, err := etcdClient.Set(name, "", 0); err != nil {
+        log.Println(err)
+        return me.logfuse("Truncate", fuse.EIO)
+    }
+    return me.logfuse("Truncate", fuse.OK)
 }
 
 func (me *EtcdFs) GetXAttr(name string, attribute string, context *fuse.Context) (data []byte, code fuse.Status) {
-        r := []byte{}
-        return r, fuse.ENOSYS
+    if me.Verbose {log.Printf("GetZAttr: %v, %v\n", name, attribute)}
+    r := []byte{}
+    return r, me.logfuse("GetXAttr", fuse.ENOSYS)
 }
 
 func (me *EtcdFs) RemoveXAttr(name string, attr string, context *fuse.Context) fuse.Status {
-        return fuse.ENOSYS
+    if me.Verbose {log.Printf("RemoveXAttr: %v, %v\n", name, attr)}
+    return me.logfuse("RemoveXAttr", fuse.ENOSYS)
 }
 
 func (me *EtcdFs) SetXAttr(name string, attr string,data []byte, flags int, context *fuse.Context) fuse.Status {
-        return fuse.ENOSYS
+    if me.Verbose {log.Printf("SetXAttr: %v, %v\n", name, attr)}
+    return me.logfuse("SetXAttr", fuse.ENOSYS)
 }
 
 func (me *EtcdFs) ListXAttr(name string, context *fuse.Context) (attrs []string, code fuse.Status) {
-        r := []string{}
-        return r, fuse.ENOSYS
+    if me.Verbose {log.Printf("ListXAttr: %v\n", name)}
+    r := []string{}
+    return r, me.logfuse("ListXAttr", fuse.ENOSYS)
 }
